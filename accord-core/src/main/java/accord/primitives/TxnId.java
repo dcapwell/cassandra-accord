0c0f9bfc5c5e2d4f6e46f84cbd9856cbb30885ff
Blake Eggleston <blake@ultrablake.com>
2023-02-02 09:40:20 -0800

    work on correctly handling range commands

--- accord-core/src/main/java/accord/primitives/TxnId.java
+++ accord-core/src/main/java/accord/primitives/TxnId.java
@@ -18,6 +18,7 @@
 
 package accord.primitives;
 
+import accord.local.Node;
 import accord.local.Node.Id;
 import accord.primitives.Routable.Domain;
 import accord.primitives.Txn.Kind;
@@ -27,6 +28,9 @@ import static accord.primitives.Txn.Kind.Write;
 
 public class TxnId extends Timestamp
 {
+//    public static final TxnId TARGET = new TxnId(1, 2, 3, new Node.Id(4));
+    public static final TxnId TARGET = new TxnId(1, 4, 3, new Node.Id(2));
+
     public static TxnId fromBits(long msb, long lsb, Id node)
     {
         return new TxnId(msb, lsb, node);
